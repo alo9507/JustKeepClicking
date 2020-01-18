54649980abc0d0b219063a78f48d76a204683860
Andrew <alo42@georgetown.edu>
2020-01-17 22:47:38 -0500

    moved Disqus below Bio

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -18,21 +18,21 @@ class BlogPostTemplate extends React.Component {
         <Helmet>
           <script defer>
             {`
-/**
- *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
- *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
- /*
- var disqus_config = function () {
- this.page.url = "https://www.justkeepclicking.io/auth/";  // Replace PAGE_URL with your page's canonical URL variable
- this.page.identifier = "auth"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
- };
- */
- (function() { // DON'T EDIT BELOW THIS LINE
- var d = document, s = d.createElement('script');
- s.src = 'https://justkeepclicking-io.disqus.com/embed.js';
- s.setAttribute('data-timestamp', +new Date());
- (d.head || d.body).appendChild(s);
- })();
+              /**
+               *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+               *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+              /*
+              var disqus_config = function () {
+              this.page.url = "https://www.justkeepclicking.io/auth/";  // Replace PAGE_URL with your page's canonical URL variable
+              this.page.identifier = "auth"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+              };
+              */
+              (function() { // DON'T EDIT BELOW THIS LINE
+              var d = document, s = d.createElement('script');
+              s.src = 'https://justkeepclicking-io.disqus.com/embed.js';
+              s.setAttribute('data-timestamp', +new Date());
+              (d.head || d.body).appendChild(s);
+              })();
           `}
           </script>
           <noscript>
@@ -81,10 +81,10 @@ class BlogPostTemplate extends React.Component {
                 marginBottom: rhythm(1),
               }}
             />
-            <div id="disqus_thread"></div>
             <footer>
               <Bio />
             </footer>
+            <div id="disqus_thread"></div>
           </article>
 
           <nav>
