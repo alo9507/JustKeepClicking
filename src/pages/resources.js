4bb084274c202c6b028441d3b36cf35b693b4d1d
Andrew <alo42@georgetown.edu>
2020-01-29 23:50:50 -0500

    added resources

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -6,7 +6,19 @@ import { rhythm } from "../utils/typography"
 const Resources = () => {
   return (
     <Layout location={"/about-me"} title={"just keep clicking"}>
-      <h1 style={{ textAlign: "center", marginTop: "0px" }}>Dev Resources</h1>
+      <h1 style={{ textAlign: "center", marginTop: "0px" }}>The Modern Dev</h1>
+
+      <h2>Learn iOS</h2>
+      <a href="https://store.raywenderlich.com/products/advanced-ios-app-architecture">
+        Advanced iOS App Architecture
+      </a>
+      <p>
+        A career-changingly good read on{" "}
+        <a href="https://developer.apple.com/videos/play/wwdc2015/408/">
+          Protocol Oriented Programming
+        </a>{" "}
+        and more with Swift.
+      </p>
 
       <h2>Learn React</h2>
       <a href="https://www.udemy.com/course/react-redux/">
@@ -25,8 +37,28 @@ const Resources = () => {
         The Linux Foundation houses the GraphQL Foundation and have kindly
         created this awesome course to get you started
       </p>
-      <br />
-      <br />
+      <h2>The Canon</h2>
+      <p>
+        <a href="https://www.amazon.com/Mythical-Man-Month-Software-Engineering-Anniversary/dp/0201835959/ref=sr_1_1?crid=3HT0ZEZ6B53EI&keywords=the+mythical+man+month&qid=1580359031&sprefix=the+mythical+man%2Caps%2C140&sr=8-1">
+          The Mythical Man Month
+        </a>
+      </p>
+      <p>
+        <a href="https://www.amazon.com/Cathedral-Bazaar-Musings-Accidental-Revolutionary/dp/0596001088/ref=sr_1_1?crid=NDMVJQ6VR94C&keywords=cathedral+and+the+bazaar&qid=1580359078&sprefix=cathedral+and+the+ba%2Caps%2C142&sr=8-1">
+          The Cathedral & the Bazaar: Musings on Linux and Open Source by an
+          Accidental Revolutionary
+        </a>
+      </p>
+      <p>
+        <a href="https://www.amazon.com/Psychology-Computer-Programming-Silver-Anniversary/dp/0932633420/ref=sr_1_1?crid=2J9MP77Q99HJ2&keywords=the+psychology+of+computer+programming&qid=1580359145&sprefix=the+psychology+of+computer+%2Caps%2C189&sr=8-1">
+          The Psychology of Computer Programming
+        </a>
+      </p>
+      <p>
+        <a href="https://www.amazon.com/Linux-Unix-Philosophy-Mike-Gancarz/dp/1555582737/ref=sr_1_1?crid=RVWTHUB5QYXD&keywords=linux+and+the+unix+philosophy&qid=1580359232&sprefix=linux+and+the+unix%2Caps%2C139&sr=8-1">
+          Linux and the Unix Philosophy
+        </a>
+      </p>
       <br />
     </Layout>
   )
